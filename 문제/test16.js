4f99823577e491b639fb2bc242cfe569f20186ad
seingyonggo <tmddyd432@gmail>
2023-03-28 15:00:15 +0900

    '23.03.28'

---
+++ 문제/test16.js
@@ -0,0 +1,9 @@
+const a = prompt("문장을 입력하세요");
+
+// a = [];
+
+// console.log(a.reverse());
+
+const reverseString = a.split("").reverse().join("");
+
+console.log(reverseString);
---
+++ 문제/test18.js
@@ -0,0 +1,5 @@
+const ko = prompt("국어점수를 입력해주세요");
+const ma = prompt("수학점수를 입력해주세요");
+const en = prompt("영어점수를 입력해주세요");
+
+console.log((ko + ma + en) / 3);
