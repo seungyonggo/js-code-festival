d91598f5d9a4775615d07a837baa6ed1e42965aa
seingyonggo <tmddyd432@gmail>
2023-04-01 00:15:33 +0900

    '23.03.31 코페19,20 문풀'

---
+++ 문제/test20.js
@@ -0,0 +1,15 @@
+// # 문제20 : 몫과 나머지
+
+// 공백으로 구분하여 두 숫자가 주어집니다.
+// 두번째 숫자로 첫번째 숫자를 나누었을 때
+// **그 몫과 나머지를 공백으로 구분하여 출력하세요.**
+
+function divide(a, b) {
+  console.log(a / b);
+  console.log(a % b);
+}
+
+const a = 5;
+const b = 2;
+const result = divide(a, b);
+// console.log(result);
