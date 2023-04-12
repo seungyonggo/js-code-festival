ee0614a754d9aaa95dd53de03f30af7271ed3677
seingyonggo <tmddyd432@gmail>
2023-04-12 22:22:21 +0900

    '23.04.12'

--- 강사님꺼/week2/javascript/q1.js
+++ 강사님꺼/week2/javascript/q1.js
@@ -6,6 +6,22 @@
 변수 d는 솔루션 함수에서 전달받은 c를 다른 형태로 바꿔준다.
 변수 e는 main함수의 최종 반환 값으로 향후 다른 함수에서 재사용된다.
 */
+function main() {
+  const a = "1";
+
+  const b = document.querySelector(".my-selector");
+
+  const c = "1";
+  const d = solution(c);
+
+  const e = "1";
+  return e;
+}
+
+function solution(input) {
+  const output = input * 2;
+  return output;
+}
 
 function solution() {}
 function parse() {}
