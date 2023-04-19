22c9138fae729e45dc7a325c973932fa6ed7b3b3
seingyonggo <tmddyd432@gmail>
2023-04-19 16:08:54 +0900

    '23.04.19 week4 dom1 문풀'

--- 강사님꺼/week3/Javascript/level1/level.js
+++ 강사님꺼/week3/Javascript/level1/level.js
@@ -1,4 +1,19 @@
-import { BANK_LIST, ACCOUNT_FORM } from './account.js';
+import { BANK_LIST, ACCOUNT_FORM } from "./account.js";
 
+const form = document.getElementById("account-send-form");
+const bankSelector = document.getElementById("bank-selector");
+const accountInput = document.getElementById("account-input");
+const accountList = document.getElementById("account-list");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const bankName = bankSelector.options[bankSelector.selectedIndex].text;
+  const accountNumber = accountInput.value;
+  const maskedAccountNumber =
+    accountNumber.substring(0, 2) + "-********-" + accountNumber.substring(10);
+  const listItem = document.createElement("li");
+  listItem.textContent = `${bankName} : ${maskedAccountNumber}`;
+  accountList.appendChild(listItem);
+});
 console.log(BANK_LIST);
 console.log(ACCOUNT_FORM);
